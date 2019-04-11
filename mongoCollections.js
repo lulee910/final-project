9d551b2001815b5f957dcab7f0f41ab233b53977
lulee910 <43452785+lulee910@users.noreply.github.com>
2019-04-11 18:13:36 -0400

    Add files via upload

---
+++ mongoCollections.js
@@ -0,0 +1,26 @@
+const dbConnection = require("./mongoConnection");
+
+const getCollectionFn = collection => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+
+module.exports = {
+    sysUser: getCollectionFn("sysUser"),
+    sysRole: getCollectionFn("sysRole"),
+    sysRoleMenu: getCollectionFn("sysRoleMenu"),
+    hjFeeInfo: getCollectionFn("hjFeeInfo"),
+    hjInfo: getCollectionFn("hjInfo"),
+    lossInfo: getCollectionFn("lossInfo"),
+    drugsInfo: getCollectionFn("drugsInfo"),
+    enterInfo: getCollectionFn("enterInfo")
+  };
