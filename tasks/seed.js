5559872fc116371ed15f940ca6012fcf65154807
lulee910@gmail.com <lulee910@gmail.com>
2019-05-18 01:41:13 -0400

    lu li

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,53 +1,41 @@
-const dbConnection = require("../config/mongoConnection");
-const doctors = require("../data/c_data_doctorInfo");
-const drugs = require("../data/d_data_drugInfo");
-const registers = require("../data/register");
+const MongoClient = require("mongodb").MongoClient;
+const crypto = require("crypto");
+const url = "mongodb://localhost:27017/project";
 
-
-dbConnection().then(
-    db => {
-        return db
-            .dropDatabase()
-            .then(() => {
-                return dbConnection;
-            })
-            .then(db => {
-                return registers.register("Phil", "123456");
-            })
-            .then(user => {
-                const serviceId = user[0].serviceId;
-                return drugs
-                    .add({ drugName: "aaa",barCode:"",drugType: "1", drugSpec:"",  drugUnit:"1", drugPrice: "11.11",SCDate : "05/17/2019", remark : "",serviceId : serviceId })
-                    .then(() => {
-                        return drugs.add({ drugName: "bbb",barCode:"",drugType: "1", drugSpec:"",  drugUnit:"2", drugPrice: "22.22",SCDate : "05/17/2019", remark : "",serviceId : serviceId });
-                    })
-                    .then(() => {
-                        return drugs.add({ drugName: "ccc",barCode:"",drugType: "2", drugSpec:"",  drugUnit:"3", drugPrice: "33.33",SCDate : "05/17/2019", remark : "",serviceId : serviceId });
-                    })
-                    .then(() => {
-                        return drugs.add({ drugName: "ddd",barCode:"",drugType: "2", drugSpec:"",  drugUnit:"1", drugPrice: "44.44",SCDate : "05/17/2019", remark : "",serviceId : serviceId });
-                    });
-            })
-            .then(drug => {
-                const serviceId = drug[0].serviceId;
-                return doctors
-                    .add({ firstName: "Eric", lastName: "Wang", tel: "111-111-1111",serviceId : serviceId })
-                    .then(() => {
-                        return doctors.add({ firstName: "Steven", lastName: "Liu", tel: "222-222-2222",serviceId : serviceId });
-                    })
-                    .then(() => {
-                        return doctors.add({ firstName: "Jack", lastName: "Chen", tel: "333-333-3333",serviceId : serviceId });
-                    });
-            })
-            .then(() => {
-                console.log("Done seeding database");
-                db.close();
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    },
-    error => {
-        console.error(error);
+MongoClient.connect(url,{ useNewUrlParser: true }, function (err, client) {
+    if (err) {
+        console.log("database connection failed");
+        return;
     }
-);
+    console.log("database connection success");
+
+    var db = client.db("project");
+    let md5 = crypto.createHash("md5");
+    let newPas = md5.update("123456").digest("hex");
+    db.dropDatabase()
+        .then(() => {
+            db.collection("sysUser").insertOne({
+                "username": "Phil",
+                "passWord": newPas,
+                "remarks": "administrator",
+                "serviceId": 1
+            })
+                .then(() => {
+                    db.collection("drugInfo").insertMany([
+                        { drugName: "aaa", barCode: "", drugType: "1", drugSpec: "", drugUnit: "1", drugPrice: "11.11", SCDate: "05/17/2019", remark: "", serviceId: 1 },
+                        { drugName: "bbb", barCode: "", drugType: "1", drugSpec: "", drugUnit: "2", drugPrice: "22.22", SCDate: "05/17/2019", remark: "", serviceId: 1 },
+                        { drugName: "ccc", barCode: "", drugType: "2", drugSpec: "", drugUnit: "3", drugPrice: "33.33", SCDate: "05/17/2019", remark: "", serviceId: 1 },
+                        { drugName: "ddd", barCode: "", drugType: "2", drugSpec: "", drugUnit: "1", drugPrice: "44.44", SCDate: "05/17/2019", remark: "", serviceId: 1 }
+                    ]).then(() => {
+                        db.collection("doctorInfo").insertMany([
+                            { firstName: "Eric", lastName: "Wang", tel: "111-111-1111", serviceId: 1 },
+                            { firstName: "Steven", lastName: "Liu", tel: "222-222-2222", serviceId: 1 },
+                            { firstName: "Jack", lastName: "Chen", tel: "333-333-3333", serviceId: 1 }
+                        ])
+                    }).then(() => {
+                        console.log("Done seeding database");
+                        client.close();
+                    })
+                })
+        })
+});
